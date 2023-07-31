0b075e13c8984e1dcf5a80751363f1bf9998ea2c
Raphael De Oliveira <Raphaeldeoliveira5640@gmail.com>
2023-07-31 11:33:32 +0200

    Ajout de la fonction dessine

--- main.c
+++ main.c
@@ -2,21 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 #include <gd.h>
+#include <math.h>
 /* @auteur = Raphael De Oliveira
+ * PROJET PIE CHART
  */
 
 const char *separator = "=:";
 
-void initialise(char* argv[], int argc, char* tab[], int tab_size){
+void initialise_tableau(char* argv[], int argc, char* tab[], int tab_size){
     int k = 0;
     for (int i = 0; i < argc && k < tab_size; i++) {
         char* strtoken = strtok(argv[i], separator);
         while (strtoken != NULL && k < tab_size) {
-            tab[k] = strdup(strtoken);
+            tab[k] = strdup(strtoken);      //Alloue la mémoire pour chaque case du tableau et insere sa valeur dedans
             k++;
             strtoken = strtok(NULL, separator);
         }
     }
+}
+
+void initialise_image(gdImagePtr *img){
+
+    *img = gdImageCreate(1000, 1000);
+    //definition des couleurs
+    int white = gdImageColorAllocate(*img,255,255,255);
+    //création de formes
+    gdImageFilledRectangle(*img, 0,0, 999 ,999,white);  //Met le fond en blanc
+
+}
+
+void telecharge_image(gdImagePtr img){
+    FILE *fichier;
+    fichier = fopen("/home/raphael/pie/pie.png", "wb");
+    gdImagePng(img, fichier);
+    fclose(fichier);
+    gdImageDestroy(img); //Destruction de l'image ici (liberation de la memoire)
 }
 
 void clean(char* tab[]){
@@ -30,27 +50,26 @@ void affiche_tab(char* tab[]){
         printf("%s\n",tab[i]);
     }
 }
-void telecharge_image(gdImagePtr *image){
-    FILE *fichier;
-    fichier = fopen("/home/raphael/pie/pie.png", "wb");
-    gdImagePng(image, fichier);
-    fclose(fichier);
-    gdImageDestroy(image);
+
+void dessine(gdImagePtr *img, char* tab[]){
+    srand( time( NULL ) );
+    int couleur_random = gdImageColorAllocate(*img, rand()%256, rand()%256, rand()%256);
+    gdImageFilledArc(*img, 500, 500, 500,500,0,200,couleur_random, gdArc);
 }
+
 int main(int argc, char* argv[]) {
     const int tab_size= strlen(*argv)*2;
     char* tab[tab_size];
 
-    initialise(argv, argc, tab, tab_size);
+    initialise_tableau(argv, argc, tab, tab_size);
 
-    gdImagePtr image = gdImageCreate(1000, 1000);
-    int red = gdImageColorAllocate(image, 255, 0, 0);
-    int green = gdImageColorAllocate(image, 0, 255, 0);
-    int blue = gdImageColorAllocate(image, 0, 0, 255);
+    gdImagePtr image;
+    initialise_image(&image);
 
-    gdImageLine(image, 1000, 0, 0, 1000, blue);
-
+    dessine(&image,tab);
     affiche_tab(tab);
+
+    //Telechargement de l'image et libération de la mémoire (image/tableaux/pointeurs)
     telecharge_image(image);
     clean(tab);
     return 0;
